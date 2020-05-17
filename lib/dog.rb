7716c64e485183c981613667a05aaee9a357f546
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-17 17:31:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,6 +28,8 @@ class Dog
     if self.id
       self.update
     else
+      
+      
       DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
       
