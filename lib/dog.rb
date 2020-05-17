1463126fab33fe435fd439281e558608caaffe79
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-17 16:31:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,12 +25,12 @@ class Dog
   end
   
   def save
-    if self.id
-      self.update
-    else
+    # if self.id
+    #   self.update
+    # else
       DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    end
+    #   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    # end
   end
   
   def self.create(name:, breed:)
